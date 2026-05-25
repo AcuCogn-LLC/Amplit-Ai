f2933e6f8ae60fedd67841be6dcedab902b12222
karanamplit <karan.amplit@gmail.com>
2026-05-25 20:58:09 +0530

    Integrate Facebook Pixel tracking in layout
    Added Facebook Pixel tracking script to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import Navigation from "@/components/navigation";
 import Footer from "@/components/footer";
 import LenisProvider from "@/components/lenis-provider";
@@ -78,6 +79,24 @@ export default function RootLayout({
         />
         <link rel="icon" href="/favicon.ico" />
       </head>
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3632920243527811');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
       <body className={`${inter.className}  text-[#000000] antialiased`}>
         <LenisProvider>
           <Navigation />
